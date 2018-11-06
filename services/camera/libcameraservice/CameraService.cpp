d57ed76148d378e4924dd0c2dd3fd2280c9decc2
chenxiao <cx@rock-chips.com>
2018-11-06 18:10:14 +0800

    modify property to vendor head name.
    Change-Id: I271700d3520e4f75624b3c8d5cf6de9c3169d21b
    Signed-off-by: chenxiao <cx@rock-chips.com>

--- services/camera/libcameraservice/CameraService.cpp
+++ services/camera/libcameraservice/CameraService.cpp
@@ -1278,7 +1278,7 @@ Status CameraService::connectHelper(const sp<CALLBACK>& cameraCb, const String8&
     binder::Status ret = binder::Status::ok();
 
     String8 clientName8(clientPackageName);
-    property_set("sys.camera.callprocess", clientName8.string());
+    property_set("vendor.sys.camera.callprocess", clientName8.string());
 
     int originalClientPid = 0;
 
